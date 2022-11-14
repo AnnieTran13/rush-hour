51c5464cec70c425113351954687855ac71ef2db
rex884 <mikeyrex884@gmail.com>
2022-11-13 23:54:42 -0500

    Created Game class

--- game.py
+++ game.py
@@ -1,1 +1,22 @@
 
+class Game:
+	#creating 2D array
+	def __init__(self,inp):
+		self.inp = inp
+		self.matrix = [["0"]*6 for i in range(0,6)]
+		n=0
+		for i in range(0,6):
+			for j in range(0,6):
+				self.matrix[i][j]=self.inp[n]
+				n+=1
+				
+		for i in range(0,6):
+			print(self.matrix[i])
+				
+c = 'BBIJ....IJCC..IAAMGDDK.MGH.KL.GHFFL.'
+Game(c)
+
+
+
+			
+	
