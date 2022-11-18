728f1cda3b292a6def184ff353aaede71f8f253d
Dann Kaoussarani <danjaf2@gmail.com>
2022-11-17 22:08:55 -0500

    Working base for right horizontal movement

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 specialCars = []
 possibleBoards=[]
+import copy
 class Car:
 	def __init__(self,letter):
 		self.letter = letter
@@ -15,59 +16,55 @@ class Car:
 		return		
 		
 	def __str__(self):
-		return self.letter
+		return self.letter + ":\n x:" + str(self.x) + "\n y:" + str(self.y)+"\n"
 		
 	def changeFuel(self,number):
 		self.fuel = number
 		return
-
-	#Generate a new board based on the possible moves (TO BE ADDED)
-	def MoveCar(self,board):
-		if(fuel!=0):
-			if(vertical):
-				Vertical(board)
-			else:
-				Horizontal(board)
-					
-					
-		return board
 		
-	def Vertical(self,board):
-		if(y==0):
-			print()
-		else if(y==5):
-			print()
-		else:
-			print()
-	
-	def Horizontal(self,board):
-		i=x[0]
-		ii = x[-1]
-		j=y[0]
-		if(i==0):
-			while(ii<6 and board[ii+1][j]=="."):
-        for a in range(ii,6):
-          self.x[a]+=1
-        
-   
-		else if(ii==5):
-      while(i>-1 and board[i-1][j]=="."):
-        for a in range(0,i):
-          self.x[a]-=1
-    
-		else:
-          print("uh oh")
 
 
+
+class BoardGen:
+	def __init__(self, cost, cars, special):
+		self.cost = cost
+		self.cars = cars
+		self.special = special
+		self.board = [["."]*6 for i in range(0,6)]
+
+		for i in range(0, 6):
+			for j in range(0, 6):
+				self.board[i][j] = '.'
+
+		for car in self.cars:
+			for i in range(0, len(car.x)):
+				self.board[car.x[i]][car.y[i]]=car.letter
+
+		print("\n")
+		for i in range(0,6):
+			print(self.board[i])
+		print("\n")
+
+
+
+
+
+def Move(self):
+	arrayMatrix = []
+	for car in self.cars:
+		car.MoveCar()
+
+	return arrayMatrix
 
 class Board:
 	#creating 2D array
 	def __init__(self,inp):
 		self.cost = 0
 		self.heuristic = 0
+		self.special=[]
 		#Seperate board from fuel amounts
 		self.inp = inp[:36]
-		self.fuel = c[37:].split(" ")
+		self.fuel = inp[37:].split(" ")
 		self.matrix = [["0"]*6 for i in range(0,6)]
 		n=0
 		for i in range(0,6):
@@ -94,7 +91,7 @@ class Board:
 		for car in self.cars:
 			for i in range(0,6):
 				for j in range(0,6):
-					if (self.matrix[i][j] == str(car)):
+					if (self.matrix[i][j] == car.letter):
 						car.x.append(i)
 						car.y.append(j)
 		#Set proper fuel level to each car
@@ -102,19 +99,40 @@ class Board:
 			for f in self.fuel:
 				if(f[0]==car.letter):
 					car.changeFuel(f[1])
-
+					self.special.append(car.letter)
 		for car in self.cars:
 			car.orientation()
-			
-		self.cars[0].MoveCar(self.matrix)
+
 	
 	def Move(self):
-		arrayMatrix = []
-		for car in self.cars:
-			car.MoveCar(tempMatrix)
-			
-		return arrayMatrix
-	
+		self.MoveCar()
+
+
+	def verticalMove(self):
+		return
+
+	def horizontalMove(self):
+		cars_copy = copy.deepcopy(self.cars)
+		for i in range(0,len(cars_copy)):
+			if(cars_copy[i].vertical==True):
+				continue
+			columm = cars_copy[i].y[-1]
+			row = cars_copy[i].x[0]
+			fuelCost = 1
+			while columm + fuelCost < 6 and self.matrix[row][columm + fuelCost] == ".":
+				cars_copy[i].y = [pos + fuelCost for pos in cars_copy[i].y]
+				generatedBoard =BoardGen(self.cost+fuelCost, cars_copy, self.special)
+				possibleBoards.append(generatedBoard)
+				cars_copy = copy.deepcopy(self.cars)
+				fuelCost += 1
+
+
+		return
+	#Generate a new board based on the possible moves (TO BE ADDED)
+	def MoveCar(self):
+		self.verticalMove()
+		print("Here")
+		self.horizontalMove()
 	
 	def __str__(self):
 		output=''
@@ -122,25 +140,10 @@ class Board:
 			output+=str(i)
 		return output
 
-class BoardGen:
-  def __init__(self,cost,cars,special,board):
-		self.cost = cost
-    self.cars = cars
-    self.special = special'
-    self.matrix = board
-  
-  def __str__(self):
-		output=''
-		for i in self.cars:
-			output+=str(i)
-		return output
-  def Move(self):
-		arrayMatrix = []
-		for car in self.cars:
-			car.MoveCar(tempMatrix)
-			
-		return arrayMatrix
-  
-			
 
-
+  
+c = 'BB......I.CC..IAAMGDDK.MGH.KL.GHFFL. B0 J2'
+game=Board(c)
+game.MoveCar()
+print(possibleBoards)
+#print(game)
